40f1b148b35e2960c0d1e9369072cc76484b0b9a
raphaelcruzeiro <raphaelcruzeiro@raphaelcruzeiro.com>
2010-12-24 18:32:33 -0200

    fixed icon for mac

--- Widget/TimeTracker/main.cpp
+++ Widget/TimeTracker/main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    a.setWindowIcon(QIcon(":/resource/army-officer-icon.png"));
+
     MainWidget mainWidget;
     mainWidget.show();
 
